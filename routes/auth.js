6c6096f8692e0112223066363a17d90f46b23cd5
matias.cincunegui@gmail.com <matias.cincunegui@gmail.com>
2015-04-13 20:06:43 -0300

    Branch para deployment en Heroku

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,16 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '478885892262648', // your App ID
         'clientSecret'  : 'c5a40304dd32d52b14272c3a9ac28c55', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'http://lotr-test.herokuapp.com/auth/facebook/callback'
     },
     'twitterAuth' : {
 		'consumerKey' 		: 'sTTVohfCvRdj8QN5kC7RrXTEV',
 		'consumerSecret' 	: 'mHUkkxBXSM6It4jqvpHydwsbgdoYKdAcsmXGtR1Fa3Svw5lYDE',
-		'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
+		'callbackURL' 		: 'http://lotr-test.herokuapp.com/auth/twitter/callback'
 	},
 	'googleAuth' : {
 		'clientID' 		: '607013064623-pp9cv1j5j7e15f77de8qrvd73g8tor71.apps.googleusercontent.com',
 		'clientSecret' 	: '7wwiMD6pu9QhlQfy5t7O7nLy',
-		'callbackURL' 	: 'http://localhost:3000/auth/google/callback'
+		'callbackURL' 	: 'http://lotr-test.herokuapp.com/auth/google/callback'
 	}
 }
