aa08f7b2a4021ca3d828a78ef4bb5433e7612c1f
Matias <matias.cincunegui@gmail.com>
2015-10-18 13:16:17 -0300

    Modificados datos para login con Twitter

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,9 @@ module.exports = {
         'callbackURL'   : 'http://lotr2.herokuapp.com/auth/facebook/callback'
     },
     'twitterAuth' : {
-		'consumerKey' 		: 'sTTVohfCvRdj8QN5kC7RrXTEV',
-		'consumerSecret' 	: 'mHUkkxBXSM6It4jqvpHydwsbgdoYKdAcsmXGtR1Fa3Svw5lYDE',
-		'callbackURL' 		: 'http://lotr-test.herokuapp.com/auth/twitter/callback'
+		'consumerKey' 		: 'LEPbc9mPKhSMcNdDpHEOd9tyu',
+		'consumerSecret' 	: 'Ok4CngY3xCQXBk7jjF48I03hqayyQwX26nciFyP3Pst15dP9nQ',
+		'callbackURL' 		: 'http://lotr2.herokuapp.com/auth/twitter/callback'
 	},
 	'googleAuth' : {
 		'clientID' 		: '607013064623-pp9cv1j5j7e15f77de8qrvd73g8tor71.apps.googleusercontent.com',
