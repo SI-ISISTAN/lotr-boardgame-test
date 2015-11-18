d181c5ce639beb8badfcf178d90933f737b52fa4
Matias <matias.cincunegui@gmail.com>
2015-11-18 18:07:11 -0300

    Agregado buscador de usuarios. Cambiados textos en la encuesta

--- admin.js
+++ admin.js
@@ -400,6 +400,7 @@ define(['https://code.jquery.com/jquery-1.8.3.js'], function(jquery){
 	});
 
 	$(document).ready(function(){
+
 		$.get( "/getconfigs", function( data ) {
 			configs = data.configs;
 			defaultConfig = data.default;
@@ -448,6 +449,29 @@ define(['https://code.jquery.com/jquery-1.8.3.js'], function(jquery){
 				$("#users-area").val(JSON.stringify(users[0],null, 4));
 			}
 	    });
+
+	    searchBox = document.querySelector("#searchBox");
+		countries = document.querySelector("#users-selector");
+		var when = "keyup"; //You can change this to keydown, keypress or change
+
+		searchBox.addEventListener("keyup", function (e) {
+		    var text = e.target.value; 
+		    var options = countries.options; 
+		    for (var i = 0; i < options.length; i++) {
+		        var option = options[i]; 
+		        var optionText = option.text; 
+		        var lowerOptionText = optionText.toLowerCase();
+		        var lowerText = text.toLowerCase(); 
+		        var regex = new RegExp("^" + text, "i");
+		        var match = optionText.match(regex); 
+		        var contains = lowerOptionText.indexOf(lowerText) != -1;
+		        if (match || contains) {
+		            option.selected = true;
+		            return;
+		        }
+		        searchBox.selectedIndex = 0;
+		    }
+		});
 
 	});
 
